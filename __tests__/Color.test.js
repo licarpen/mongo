9afd3696f34ec21b117911fc6bc58972fcdca871
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-05 14:55:59 -0800

    passing tests for color model

--- __tests__/Color.test.js
+++ __tests__/Color.test.js
@@ -6,12 +6,28 @@ describe('Color model', () => {
     expect(color.validateSync().errors.name.message)
       .toEqual('Path `name` is required.');
   });
-  it('has a required red value below 256', () => {
+  it('red has required number type', () => {
+    const color = new Color({
+      name: 'red',
+      red: 'bologne'
+    });
+    expect(color.validateSync().errors.red.message)
+      .toEqual('Cast to Number failed for value \"bologne\" at path \"red\"');
+  });
+  it('has a required red value at or above 0', () => {
     const color = new Color({
       name: 'red',
       red: -1
     });
     expect(color.validateSync().errors.red.message)
       .toEqual('Path `red` (-1) is less than minimum allowed value (0).'); 
+  });
+  it('has a required red value below 256', () => {
+    const color = new Color({
+      name: 'red',
+      red: 256
+    });
+    expect(color.validateSync().errors.red.message)
+      .toEqual('Path `red` (256) is more than maximum allowed value (255).'); 
   });
 });
