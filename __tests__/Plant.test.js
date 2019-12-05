aae6738901579a8caf942a9a30e01ea08bf90572
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-05 15:23:24 -0800

    passing all tests for plant model

--- __tests__/Plant.test.js
+++ __tests__/Plant.test.js
@@ -3,40 +3,36 @@ const Plant = require('../lib/models/Plant');
 describe('Plant model', () => {
   it('has a required name', () => {
     const plant = new Plant({});
-    expect(color.validateSync().errors.name.message)
+    expect(plant.validateSync().errors.name.message)
       .toEqual('Path `name` is required.');
   });
   it('water has required number type', () => {
     const plant = new Plant({
-      name: 'Snake',
       water: 'bologne'
     });
-    expect(plant.validateSync().errors.red.message)
-      .toEqual('Cast to Number failed for value \"bologne\" at path \"red\"');
+    expect(plant.validateSync().errors.water.message)
+      .toEqual('Cast to Number failed for value \"bologne\" at path \"water\"');
   });
   it('light has required string type', () => {
     const plant = new Plant({
-      name: 'Snake',
-      water: 2,
       light: {}
     });
     expect(plant.validateSync().errors.light.message)
-      .toEqual('Cast to Number failed for value \"bologne\" at path \"red\"');
+      .toEqual('Cast to String failed for value \"{}\" at path \"light\"');
   });
   it('has a required pH value at or above 0', () => {
     const plant = new Plant({
-      name: 'red',
       pH: -1
     });
-    expect(plant.validateSync().errors.red.message)
-      .toEqual('Path `red` (-1) is less than minimum allowed value (0).'); 
+    console.log(plant.validateSync());
+    expect(plant.validateSync().errors.pH.message)
+      .toEqual('Path `pH` (-1) is less than minimum allowed value (0).'); 
   });
   it('has a required pH value below 14', () => {
     const plant = new Plant({
-      name: 'red',
       pH: 15
     });
-    expect(color.validateSync().errors.red.message)
-      .toEqual('Path `red` (256) is more than maximum allowed value (255).'); 
+    expect(plant.validateSync().errors.pH.message)
+      .toEqual('Path `pH` (15) is more than maximum allowed value (14).'); 
   });
 });
