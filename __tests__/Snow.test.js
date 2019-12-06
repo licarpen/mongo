24386b2bb1c20e9bc2f09a12698f088b3b5817ca
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-05 21:44:03 -0800

    passing all snow tests

--- __tests__/Snow.test.js
+++ __tests__/Snow.test.js
@@ -15,30 +15,29 @@ describe('Snow model', () => {
   });
   it('has required moistureContent', () => {
     const snow = new Snow({
-      grade: 'v-hard'
     });
     expect(snow.validateSync().errors.moistureContent.message)
-      .toEqual('Cast to Number failed for value \"v-hard\" at path \"grade\"');
+      .toEqual('Path `moistureContent` is required.');
   });
   it('moisture content is at or above 0', () => {
     const snow = new Snow({
       moistureContent: -3
     });
-    expect(snow.validateSync().errors.grade.message)
-      .toEqual('Path `grade` (5) is less than minimum allowed value (5.5).'); 
+    expect(snow.validateSync().errors.moistureContent.message)
+      .toEqual('Path `moistureContent` (-3) is less than minimum allowed value (0).'); 
   });
   it('moisture content is not above 100', () => {
     const snow = new Snow({
       moistureContent: 1000
     });
-    expect(snow.validateSync().errors.grade.message)
-      .toEqual('Path `grade` (5.17) is less than minimum allowed value (5.5).'); 
+    expect(snow.validateSync().errors.moistureContent.message)
+      .toEqual('Path `moistureContent` (1000) is more than maximum allowed value (100).'); 
   });
   it('faceted is type boolean', () => {
     const snow = new Snow({
       faceted: 'Yes! You gonna die if you hike Glory Bowl today!'
     });
     expect(snow.validateSync().errors.faceted.message)
-      .toEqual('Path `grade` (5.17) is less than minimum allowed value (5.5).'); 
+      .toEqual('Cast to Boolean failed for value \"Yes! You gonna die if you hike Glory Bowl today!\" at path \"faceted\"'); 
   });
 });
